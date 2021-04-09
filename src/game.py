645a30ac325832790547e1217af02c33218237d0
Jonathan-R0 <jrosenblatt@fi.uba.ar>
2021-04-09 13:02:31 -0300

    Clean code.

--- src/game.py
+++ src/game.py
@@ -1,4 +1,88 @@
-# Complete to make the main file smaller and stop passing so many things from function to function.
+import sys
+import pygame
+from timer import Timer
+from window import Window
+from table import Table
+
+SCREEN_WIDTH = 833
+SCREEN_HEIGHT = 500
+
+TADA = "./../assets/tada.wav"
+FART = "./../assets/fart.wav"
+FPS = 30
+
 
 class Game:
-    pass
+    def __init__(self):
+        self.window = Window(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.cursor = self.window.create_cursor()
+        self.table = Table()
+        self.running = True
+        self.game_ended = False
+        self._starting_screen()
+        self.timer = Timer(self.cursor.display, (700, 350),
+                           self.window.mixer.Sound("./../assets/pop.wav"))
+
+    def _starting_screen(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    return
+
+            self.window.display.blit(
+                pygame.image.load("./../assets/startingscreen.png"), (0, 0))
+            self.window.update()
+
+    def detect_event(self, event):
+        if event.key == pygame.K_DOWN or event.key == ord('s'):
+            self.cursor.go_down()
+        elif event.key == pygame.K_LEFT or event.key == ord('a'):
+            self.cursor.go_left()
+        elif event.key == pygame.K_UP or event.key == ord('w'):
+            self.cursor.go_up()
+        elif event.key == pygame.K_RIGHT or event.key == ord('d'):
+            self.cursor.go_right()
+        elif event.key == pygame.K_RETURN:
+            return self.cursor.hit(self.table)
+        self.cursor.draw()
+        return False
+
+    def draw(self):
+        self.timer.update()
+        self.window.draw_background()
+        self.timer.draw()
+        self.table.draw(self.window.display, self.cursor.width,
+                        self.cursor.height)
+        self.cursor.draw()
+
+    def play_ending_sound(self, song):
+        sound = self.window.mixer.Sound(song)
+        self.draw()
+        self.window.update()
+        channel = sound.play()
+        while channel.get_busy():
+            pygame.time.wait(100)
+
+    def game_loop(self):
+        while self.running:
+
+            self.draw()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                elif event.type == pygame.KEYDOWN and not self.game_ended:
+                    if self.detect_event(event) and not self.table.i_win():
+                        self.table.enemy_pick()
+                pygame.display.update()
+
+            if self.table.i_win():
+                self.running = False
+                self.play_ending_sound(TADA)
+            if self.table.i_lose():
+                self.running = False
+                self.play_ending_sound(FART)
+
+            self.window.clock.tick(FPS)
+            self.window.update()
--- src/main.py
+++ src/main.py
@@ -1,100 +1,10 @@
-import sys
 import logging
 import traceback
-import pygame
-from timer import Timer
-from window import Window
-from table import Table
-
-SCREEN_WIDTH = 833
-SCREEN_HEIGHT = 500
-TADA = "./../assets/tada.wav"
-FART = "./../assets/fart.wav"
-FPS = 30
-
-
-def starting_screen(display, window):
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                return
-
-        display.blit(pygame.image.load("./../assets/startingscreen.png"),
-                     (0, 0))
-        window.update()
-
-
-def detect_event(cursor, event, table):
-    if event.key == pygame.K_DOWN or event.key == ord('s'):
-        cursor.go_down()
-    elif event.key == pygame.K_LEFT or event.key == ord('a'):
-        cursor.go_left()
-    elif event.key == pygame.K_UP or event.key == ord('w'):
-        cursor.go_up()
-    elif event.key == pygame.K_RIGHT or event.key == ord('d'):
-        cursor.go_right()
-    elif event.key == pygame.K_RETURN:
-        return cursor.hit(table)
-    #elif event.key == pygame.K_BACKSPACE:
-    #    cursor.unhit(table)
-    cursor.draw()
-    return False
-
-
-def draw(timer, window, table, cursor):
-    timer.update()
-    window.draw_background()
-    timer.draw()
-    table.draw(window.display, cursor.width, cursor.height)
-    cursor.draw()
-
-
-def play_ending_sound(timer, window, table, cursor, song):
-    sound = window.mixer.Sound(song)
-    draw(timer, window, table, cursor)
-    window.update()
-    channel = sound.play()
-    while channel.get_busy():
-        pygame.time.wait(100)
-
-
-def run():
-    window = Window(SCREEN_WIDTH, SCREEN_HEIGHT)
-    cursor = window.create_cursor()
-    table = Table()
-    running = True
-    game_ended = False
-    starting_screen(cursor.display, window)
-    timer = Timer(cursor.display, (700, 350),
-                  window.mixer.Sound("./../assets/pop.wav"))
-
-    while running:
-
-        draw(timer, window, table, cursor)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN and not game_ended:
-                if detect_event(cursor, event, table) and not table.i_win():
-                    table.enemy_pick()
-            pygame.display.update()
-
-        if table.i_win():
-            running = False
-            play_ending_sound(timer, window, table, cursor, TADA)
-        if table.i_lose():
-            running = False
-            play_ending_sound(timer, window, table, cursor, FART)
-
-        window.clock.tick(FPS)
-        window.update()
-
+from game import Game
 
 if __name__ == "__main__":
     try:
-        run()
+        game = Game()
+        game.game_loop()
     except Exception as error:
         logging.error(traceback.format_exc())
